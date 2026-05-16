1ab5e574cf26ac6ec1f8219ed6ec9d8fefd4247c
ivan <iokhabsky@gmail.com>
2026-05-16 09:32:08 +0300

    dashboard desktop

--- js/app.js
+++ js/app.js
@@ -1,3 +1,46 @@
-window.onload = function() {
-    console.log("loaded")
+window.onload = function () {
+    console.log("loaded");
+
+    // cards slider
+    const swiper = new Swiper('.card ', {
+        // Optional parameters
+        slidesPerView: 1,
+        speed: 800,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true,
+        },
+        direction: 'horizontal',
+        loop: true,
+
+        // If we need pagination
+        // pagination: {
+        //     el: '.swiper-pagination',
+        // },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        // },
+    });
+    //
+
+    // card switcher
+
+    const switcher = document.querySelector(".switcher");
+
+    switcher.addEventListener('click', () => {
+        switcher.classList.toggle('active');
+        if(switcher.id === 'deactivateCard') {
+            switcher.parentElement.parentElement.classList.toggle('hidden');
+        }
+    });
+    
+    //
 }
